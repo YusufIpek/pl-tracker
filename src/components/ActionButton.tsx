9d3d7ae006a8030c163e12e21b0a9c4ead4d95eb
Yusuf Ipek <yusuf.ipek2@gmail.com>
2022-03-26 12:21:08 +0100

    add button updated

--- src/components/ActionButton.tsx
+++ src/components/ActionButton.tsx
@@ -10,7 +10,7 @@ export default function ActionButton(props: Props) {
   return (
     <button
       className={
-        'flex justify-center text-3xl items-center cursor-pointer hover:scale-110 transition-transform duration-600 ease-in-out active:shadow-xl shadow-gray-900 w-[70px] h-[70px] bg-green-600 text-white rounded-full ' +
+        'flex justify-center text-3xl items-center cursor-pointer hover:scale-110 transition-transform duration-600 ease-in-out shadow-lg shadow-gray-900 w-[70px] h-[70px] bg-green-600 text-white rounded-full ' +
         props.className
       }
       onClick={props.onClick}
